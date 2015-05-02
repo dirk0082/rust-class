7c77c97a03468173ac51bbfbc4c509e23438acee
Steve Tuckner <stevetuckner@Steves-MacBook-Air.local>
2015-05-02 11:27:05 -0500

    fixed up fib

--- hello.rs
+++ hello.rs
@@ -1,14 +1,21 @@
-fn fib(n: i32) {
-  if n == 0 && n == 1 {
-    1;
+fn fib(n: i32) -> i32 {
+  if n == 0 || n == 1 {
+    1
+  }
+  else if n > 2^30 {
+    println!("overflow");
+    0
   }
   else {
-    fib(n-1) + fib(n-2);
+    //let n1 = fib(n-1);
+    //let n2 = fib(n-2);
+    //n1 + n2;
+    fib(n-1) + fib(n-2)
   }
 }
 
 fn main() {
-  let val = 100;
+  let val = 10;
   println!("fib of {0} is {1:?}", val, fib(val));
 }
 
