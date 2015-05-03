1324e56101ba5b671260c64f976cfeb68cc7af42
Paul <pauldirks@Paul-Dirkss-MacBook-Pro.local>
2015-05-03 11:06:00 +0200

    added hello

---
+++ hello.rs
@@ -0,0 +1,13 @@
+fn fib(n: i32) {
+  if n == 0 && n == 1 {
+    1;
+  }
+  else {
+    fib(n-1) + fib(n-2);
+  }
+}
+
+fn main() {
+  let val = 100;
+  println!("fib of {0} is {1:?}", val, fib(val));
+}
